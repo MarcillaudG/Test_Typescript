e7bc916f1e73c3a94728446f15f3728f14dd1da2
MarcillaudG <guilhem.marcillaud@gmail.com>
2022-07-14 00:28:31 +0200

    TASK-1005 Login User

--- controllers/users/ManipulateUserController.ts
+++ controllers/users/ManipulateUserController.ts
@@ -12,6 +12,31 @@ export default class ManipulateUserController {
         this.userModel = userModel
         router.get('/api/users',
             this.listUsers.bind(this))
+
+
+        this.router.route({
+            method: 'GET',
+            url: '/api/users/login',
+            schema: {
+                querystring: {
+                    name: { type: 'string' },
+                    password: {type: 'string'}
+                }
+            },
+            handler: async (request, reply) => {
+                let splitted = request.url.split("?")
+                const name = splitted[1].split("=")[1]
+                //const token = generateString(Number(process.env.TOKEN_SIZE))
+                const token = generateString(10)
+                const access:JSON = <JSON><unknown>{
+                    "name": name,
+                    "token": token
+                }
+                reply.send(access)
+            }
+        })
+
+
         
     }
 
