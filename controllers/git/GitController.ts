67579d3bb5d4e53b86bfdc1318defb27754b558d
MarcillaudG <guilhem.marcillaud@gmail.com>
2022-07-14 11:12:53 +0200

    TASK-1007 and 1008 - Use of axios

--- controllers/git/GitController.ts
+++ controllers/git/GitController.ts
@@ -1,5 +1,6 @@
 import {FastifyInstance} from "fastify";
 import { Octokit } from "@octokit/rest";
+const axios = require('axios').default;
 
 
 
@@ -14,58 +15,43 @@ export default class GitController {
         router.get('/api/github/feed',
             this.gitFeed.bind(this))
 
-            this.router.route({
-                method: 'GET',
-                url: '/api/github/users/*',
-                schema: {
-                    querystring: {
-                        name: { type: 'string' },
-                        password: {type: 'string'}
-                    }
-                },
-                handler: async (request, reply) => {
-                    let splitted = request.url.split("/")
-                    const user = splitted[splitted.length-1]
-    
-                    const octokit = new Octokit({
-                        auth: process.env.TOKEN_GITHUB
-                      })
-                    const response = await octokit.request('GET /user/{username}', {
-                        username: "MarcillaudG"
-                    })
-                    /*
-                    let res:String[] = []
-                    response.data.forEach((obj: { id: any; login: any; avatar_url: any; public_repos: any; public_gists: any; followers: any; following: any; }) => {
-                        res.push(` 
-                        "id": ${obj.id},
-                        "login": ${obj.login},
-                        "avatar": ${obj.avatar_url},
-                        "details":
-                            { 
-                            "public_repos": ${obj.public_repos}
-                            "public_gists": ${obj.public_gists}
-                            "followers": ${obj.followers}
-                            "following": ${obj.following}
-                        }`)
-                    })*/
-                    reply.send("Hello")
+        this.router.route({
+            method: 'GET',
+            url: '/api/github/users/*',
+            schema: {
+                querystring: {
+                    name: { type: 'string' },
+                    password: {type: 'string'}
                 }
-            })
+            },
+            handler: async (request, reply) => {
+                let splitted = request.url.split("/")
+                const user = splitted[splitted.length-1]
+
+                let api = 'https://api.github.com/users/' + user
+                let github = ""
+                axios.get(api).then(function(response: { data: any; }) {
+                github = response.data;
+                reply.send(JSON.stringify(github))
+                });
+            }
+        })
     }
 
 
     async gitFeed(): Promise<String> {
 
-        const octokit = new Octokit({
-            auth: process.env.TOKEN_GITHUB
-          })
-        const response = await octokit.request('GET /events', {})
-        let res:String[] = []
-        response.data.forEach(obj => {
-            res.push(` { "type": "${String(obj.type)}, "actor": { "id": ${String(obj.actor.id)}, "login": ${String(obj.actor.login)} }, "repo" : { "id": ${String(obj.repo.id)}, "name": ${String(obj.repo.name)}}}`)
-        });
-        return res.toString()
+        let res = "Hello"
+        const response = await axios.get('https://api.github.com/events', {
+            method: "GET",
+            headers: {
+              'Authorization': `token ${process.env.TOKEN_GITHUB}`,
+            }
+          });
+        console.log(await response);
+        return JSON.stringify(response.data)
     }
+
 
 
 }
